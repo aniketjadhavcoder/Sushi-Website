519225cd934c0acce809fdf999d27adff38e9d36
aniketjadhavcoder <131300760+aniketjadhavcoder@users.noreply.github.com>
2023-06-29 22:26:00 +0530

    new aos

--- js/script.js
+++ js/script.js
@@ -5,8 +5,7 @@ import sushi12 from '../sushi_assets/assets/sushi-12.png'
 import sushi11 from '../sushi_assets/assets/sushi-11.png'
 import sushi10 from '../sushi_assets/assets/sushi-10.png'
 
-import AOS from "aos";
-import "aos/dist/aos.css";
+
 
 // AOS file generate with npm install aos
 // init AOS animation
